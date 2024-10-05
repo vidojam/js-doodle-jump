fe1623488441506977796c9936a6ce46e29df2d3
Jose Torres <vidojam@gmail.com>
2024-10-04 21:59:19 -0400

    jump coding

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
     const grid = document.querySelector('.grid');
     const doodler = document.createElement('div');
     let isGameOver = false;
+    let speed = 3;
     const platformCount = 5; // Use const if it's not going to change
     let platforms = [];
     let score = 0;
     let doodlerLeftSpace = 50;
-    let doodlerBottomSpace = 150;
+    let startPoint = 150;
+    let doodlerBottomSpace = startPoint;
+    let isJumping = true;
+    let gravity = 0.9;
+    let upTimerId;
+    let downTimerId;
+    let isGoingLeft = false;
+    let isGolingRight = false;
+    let leftTimerId;
+    let rightTimerId;
+
 
     class Platform {
         constructor(newPlatBottom) {
@@ -54,6 +65,54 @@ function movePlatforms() {
         doodlerLeftSpace = platforms[0].left;
         doodler.style.left = doodlerLeftSpace + 'px';
         doodler.style.bottom = doodlerBottomSpace + 'px';
+    }
+
+    function fall() {
+        isJumping = false;
+        clearInterval(upTimerId);
+        downTimerId = setInterval(function() {
+            doodlerBottomSpace -= 5;
+            doodler.style.bottom = doodlerBottomSpace + 'px';
+            if (doodlerBottomSpace <= 0) {
+                gameOver();
+            }
+            platforms.forEach(platform => {
+                if (
+                    (doodlerBottomSpace >= platform.bottom) &&
+                    (doodlerBottomSpace <= platform.bottom + 15) &&
+                    ((doodlerLeftSpace + 60) >= platform.left) &&
+                    (doodlerLeftSpace <= (platform.left + 85)) &&
+                    !isJumping
+                ) {
+                    console.log('landed');
+                    jump();
+                }
+            });
+        }, 20);
+    }
+    fall();
+
+    function jump() {  
+        clearInterval(downTimerId);
+        isJumping = true;
+        upTimerId = setInterval(function() {
+            doodlerBottomSpace += 20;
+            doodler.style.bottom = doodlerBottomSpace + 'px';
+            if (doodlerBottomSpace > startPoint + 200) {
+                fall();
+                isJumping = false;
+            }
+        }, 30);
+    }
+
+    function gameOver() {
+        isGameOver = true;
+        while (grid.firstChild) {
+            grid.removeChild(grid.firstChild);
+        }
+        grid.innerHTML = score;
+        clearInterval(downTimerId);
+     
     }
 
     function start() {
